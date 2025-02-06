ee6a69295596ec88a517f5b27711419fdf85ce57
Mohit Bhimrajka <mohitbhimrajka5@gmail.com>
2025-02-06 10:31:29 +0530

    Modified Calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 def add(a, b):
 	return a + b
 
-a = int(input("Please enter the first number"))
+a = int(input("Please enter the first number: "))
 
-b = int(input("Please enter the second number"))
+b = int(input("Please enter the second number: "))
 
 print("The sum of your numbers is", add(a, b))
