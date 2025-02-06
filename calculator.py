ad373f27a4ceae575faa3005959fdbd50e16815d
MohitBhimrajka <mohitbhimrajka5@gmail.com>
2025-02-06 11:12:08 +0530

    Added subtract function

--- calculator.py
+++ calculator.py
@@ -3,8 +3,13 @@
 def add(a, b):
 	return a + b
 
+
+def subtract(a, b):
+	return a - b
+
 a = int(input("Please enter the first number: "))
 
 b = int(input("Please enter the second number: "))
 
 print("The sum of your numbers is", add(a, b))
+print("The difference of your numbers is", subtract(a, b))
